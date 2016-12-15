c1bdbdc7fd86577d04a3d8dddd61fc0331aa211f
AokiCE <achiharu263@gmail.com>
2016-12-15 11:06:36 +0900

    Update scale.fix.js
    changed side nav bar style

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -16,12 +16,14 @@ function gestureStart() {
   }
 }
 
-/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
