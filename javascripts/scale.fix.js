17c63fca7a74519970a37cb07841ac7ba6a16909
AokiCE <achiharu263@gmail.com>
2016-12-15 11:04:47 +0900

    Update scale.fix.js
    nav side bar

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,3 +15,13 @@ function gestureStart() {
     }
   }
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
